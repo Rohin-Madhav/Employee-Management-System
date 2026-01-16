40dae05203b4823b611e7dccba182930f936b5fd
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-01-16 19:24:31 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import ManageEmploye from "./pages/ManageEmploye";
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-blue-500'> HELLO WORLD</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="manage" element={<ManageEmploye />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/ManageEmploye.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageEmploye = () => {
+    return (
+        <div>
+            Manage
+        </div>
+    );
+}
+
+export default ManageEmploye;

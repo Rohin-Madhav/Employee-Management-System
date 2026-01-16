aec6431806f97753b7e280f3178bd668c3a20460
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-01-17 00:55:48 +0530

    working on employe managing page

--- frontend/src/pages/ManageEmploye.jsx
+++ frontend/src/pages/ManageEmploye.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import { useState,useEffect } from "react";
+import api from "../service/Api"
+
 
 const ManageEmploye = () => {
-    return (
-        <div>
-            Manage
-        </div>
-    );
-}
+    const [employe, setEmploye] = useState([]);
+
+  useEffect(() => {
+    const fetchEmploye = async () => {
+      try {
+        const res = await api.get("/active");
+      setEmploye(res.data.employes);
+      } catch (error) {
+        console.log(error,"Employe fetching err"); 
+      }
+    };
+    fetchEmploye();
+  }, []);
+  return <div>
+    <div>
+        <h1>Employe Details</h1>
+    </div>
+    <div>
+         <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Salary</th>
+            <th>Status</th>
+            <th>Actions</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+         {employe.map((e)=>(
+             <tr key={e._id} > 
+              <td>{e._id}</td>
+              <td>{e.name}</td>
+              <td>{e.department}</td>
+              <td>{e.salary}</td>
+              <td>{e.status}</td>
+              <td>
+  <button>view</button>
+  <button>Edit</button>
+  <button>Delete</button>
+
+              </td>
+
+            </tr>
+         ))}
+
+        </tbody>
+      </table>
+    </div>
+  </div>;
+};
 
 export default ManageEmploye;
